6073cee0d874165271260a71964234142830ca28
kun <kunl@pku.edu.cn>
2019-10-02 16:46:19 +0900

    modify cpp to make a Socketclass

--- SockClientTest/SockClientTest.cpp
+++ SockClientTest/SockClientTest.cpp
@@ -5,135 +5,95 @@
 #include <stdio.h>  
 #include <boost/thread.hpp>
 #include <Winsock2.h>  
-
+/*
 #pragma comment(lib,"ws2_32.lib")
 
 #pragma warning(disable:4996)
-
+*/
 #include <iostream>
-
-int Sender() {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	wVersionRequested = MAKEWORD(1, 1);
-
-	err = WSAStartup(wVersionRequested, &wsaData);
-
-	if (err != 0) {
-
-		return -1;
-
-	}
-
-	if (LOBYTE(wsaData.wVersion) != 1 ||
-
-		HIBYTE(wsaData.wVersion) != 1) {
-
-		WSACleanup();
-
-		return -1;
-
-	}
-
-	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
-	SOCKADDR_IN addrSrv;
-
-	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-
-	addrSrv.sin_family = AF_INET;
-
-	addrSrv.sin_port = htons(8889);
-
-	connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));
-
-	const char baseCh[] = "hello, world!";
-	for (int clc = 0; clc < 5; clc++)
-	{
-		send(sockClient, baseCh, strlen(baseCh) + 1, 0);
-
-		char recvBuf[50];
-
-		recv(sockClient, recvBuf, 50, 0);
-
-		printf("severRecv %s\n", recvBuf);
-
-		char t[2];
-
-		std::cin >> t;
-
-	}
-
-	closesocket(sockClient);
-	WSACleanup();
-
-	return 0;
-}
-
-int Reciever() {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	wVersionRequested = MAKEWORD(1, 1);
-
-	err = WSAStartup(wVersionRequested, &wsaData);
-
-	if (err != 0) {
-		return -1;
-	}
-
-	if (LOBYTE(wsaData.wVersion) != 1 ||
-		HIBYTE(wsaData.wVersion) != 1) {
-		WSACleanup();
-		return -1;
-
-	}
-
-	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
-	SOCKADDR_IN addrSrv;
-
-	addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-
-	addrSrv.sin_family = AF_INET;
-
-	addrSrv.sin_port = htons(8890);
+class MyClientSocket {
+public:
 	
-	try
-	{
-		int connectCount = 0;
-		while (connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR)) != 0) {
-			printf("connecting at No.%d \n", ++connectCount);
-			Sleep(3);
+	MyClientSocket() {
+
+		WORD wVersionRequested;
+		WSADATA wsaData;
+		int err;
+		wVersionRequested = MAKEWORD(1, 1);
+
+		err = WSAStartup(wVersionRequested, &wsaData);
+
+		if (err != 0) {
+			return;
 		}
-		printf("connected!\n");
-		const char baseCh[] = "hello, this is message from cpp!";
-		send(sockClient, baseCh, strlen(baseCh) + 1, 0);
-		int ret = 0;
-		do
+
+		if (LOBYTE(wsaData.wVersion) != 1 ||
+			HIBYTE(wsaData.wVersion) != 1) {
+			WSACleanup();
+			return;
+		}
+
+		sockClient = socket(AF_INET, SOCK_STREAM, 0);
+		SOCKADDR_IN addrSrv;
+
+		addrSrv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+		addrSrv.sin_family = AF_INET;
+
+		addrSrv.sin_port = htons(8890);
+
+		try
 		{
+			int connectCount = 0;
+			while (connect(sockClient, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR)) != 0) {
+				printf("connecting at No.%d \n", ++connectCount);
+				Sleep(3);
+			}
+			printf("connected!\n");
 
-			char recvBuf[50] = "";
+		}
+		catch (const std::exception&)
+		{
+			closesocket(sockClient);
+			WSACleanup();
+		}
 
-			ret = recv(sockClient, recvBuf, 50, 0);
+	};
 
-			printf("Receive:%s\n", recvBuf);
+	 int receiveMsg() {
+		 // wait for the msg until receive NULL msg
+		 const char baseCh[] = "hello, this is message from cpp!";
+		 send(sockClient, baseCh, strlen(baseCh) + 1, 0);
+		 int ret = 0;
+		 do
+		 {
+			 char recvBuf[50] = "";
 
-		} while (ret > 0);
-	}
-	catch (const std::exception&)
-	{
-		closesocket(sockClient);
-		WSACleanup();
-	}
-	return 0;
-}
+			 ret = recv(sockClient, recvBuf, 50, 0);
+
+			 printf("Receive:%s\n", recvBuf);
+
+		 } while (ret > 0);
+		 return 1;
+	 }
+
+	 ~MyClientSocket() {
+		 closesocket(sockClient);
+		 WSACleanup();
+	 }
+private:
+	SOCKET sockClient = { 0 };
+};
+
+
 
 int main()
 {
 
 	
 	//boost::thread thr(Reciever);
-	Reciever();
+	MyClientSocket myClientSocket;
+	myClientSocket.receiveMsg();
 	getchar();
 	return 0;
 
--- python/selfAdaption/communication/mysocket.py
+++ python/selfAdaption/communication/mysocket.py
@@ -5,20 +5,21 @@ class MySocket:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             server.bind(("localhost", 8890))
+            print("Connecting")
             server.listen(0)
             connection, address = server.accept()
             self.connection = connection
 
-            print(connection, address)
-            recv_str = connection.recv(1024)[0:5]
+
+            recv_str = connection.recv(1024)
             recv_str = recv_str.decode("ascii")
-            print("Recive:\n", recv_str)
+            print("Connected:", connection, address)
+            print("And recive:\n", recv_str)
             
         except socket.error as msg:
             print("Couldnt connect with the socket-server: %s\n terminating program" % msg)
             connection.close()
 
-        input("enter end")
 
     def __del__(self):
         print("Deleting class and close socket connection.")
@@ -32,4 +33,6 @@ class MySocket:
 if __name__=="__main__":
     obj = MySocket()
     print("this is", obj)
-    obj.send("this is server")
+    while 1:
+        s = input()
+        obj.send(s if s!="end" else "")
